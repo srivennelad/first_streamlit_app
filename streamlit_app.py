5fbc49cd9b32b4c09be74bd70d58ff3568b37d00
srivennelad <81457115+srivennelad@users.noreply.github.com>
2023-04-04 13:41:58 +0530

    Update streamlit_app.py
    removed errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-   🥑🍞
+
 streamlit.header('Breakfast Favourites!')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
